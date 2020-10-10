b2389acbb6c3fcfdb470e88e11ed755d03df51d3
jayeshrathi6055 <65055605+jayeshrathi6055@users.noreply.github.com>
2020-10-10 20:44:47 +0530

    Add files via upload

---
+++ main1.cpp
@@ -0,0 +1,119 @@
+# include<iostream>
+# include<iomanip>
+using namespace std;
+
+int main(){
+	
+////	Manipulators---------------------------------------------------------->
+//	int a = 12;
+//	cout<<"withot setw - "<<a;
+//	cout<<endl<<"with setw"<<setw(4)<<12;
+	
+////	If - Else statement--------------------------------------------------->
+//	int i = 6;
+//	if (i<5){
+//		cout<<endl<<i;
+//	}
+//	else {
+//		cout<<endl<<"Not valid";
+//	}
+	
+////	IF-ELSE Ladder ------------------------------------------------------->
+//	int age;
+//	cout <<endl<<"Enter your age - "<<endl;
+//	cin >>age;
+//	
+//	if (age<18){
+//		cout<<endl<<"You are under 18";
+//	}
+//	else if (age==18){
+//		cout<<endl<<"Your age is 18.";
+//	}
+//	else{
+//		cout<<endl<<"You are 18+.";
+//	}
+
+////	Switch case ------------------------------------------------------------------>
+//	int age;
+//	cout <<endl<<"Enter your age - "<<endl;
+//	cin >>age;
+//
+//	switch (age){
+//		case 10 :
+//			cout<<"Bacha";
+//			break;
+//		
+//		case 20:
+//			cout<<"thoda bada";
+//			break;
+//		
+//		default:
+//			cout<<"No special cases";
+//			break;
+//	}	
+
+////	For loop ------------------------------------------------------------------------->
+//	for (int i = 0;i<11;i++){
+//		cout<<i<<endl;
+//	}
+
+// //	while loop----------------------------------------------------------------------------->
+// 	int i = 0;
+// 	while (i<10){
+// 		cout<<i<<endl;
+// 		i++;
+// 	}
+
+////	Do-While loop--------------------------------------------------------------------------->
+//	int i = 0;
+//	do{
+//		cout<<i<<endl;
+//		i++;
+//	}while(i<11);
+
+////	Pointer------------------------------------------------------------------------------------->
+////	It is a datatype which holds addresses of other datatypes.
+//	int a = 2;
+//	int* b = &a;
+////	& ------> address of
+////	* ------> dereference operator
+////	cout<<&a; ----------> It shows address of a
+////	cout <<*b; ---------> It gives value of a
+////	cout<<b; -----------> give similar value as &a
+
+////	Pointer to Pointer----------------------------------------------------------------------------->
+//	int a = 2;
+//	int* b = &a;
+//	int**c = &b;
+//	cout<<c<<endl;
+//	cout<<*c<<endl;
+//	cout<<**c<<endl;
+
+////	Array-------------------------------------------------------------------------------------------->
+//	int a[4] = {11,21,31,41};
+//	cout<<a<<endl;
+//	cout<<a[0]<<endl;
+//	cout<<a[1]<<endl;
+//	cout<<a[2]<<endl;
+//	cout<<a[3]<<endl;
+//	
+//	int b[3];
+//	b[0] = 78;
+//	b[1] = 68;
+//	b[2] = 58;
+//	cout<<b[0]<<endl;
+//	cout<<b[1]<<endl;
+//	cout<<b[2]<<endl;
+
+// //	Pointer with Arrays----------------------------------------------------------------------------------->
+// 	int a[4] = {11,21,31,41};
+// 	int *b = a;
+// 	cout<<b<<endl;
+// 	cout<<*b<<endl;
+// 	cout<<*(b+1)<<endl;       // We can also use b++ or ++b
+// 	cout<<*(b+2)<<endl;
+// 	cout<<*(b+3)<<endl;
+
+	
+	return 0;
+}
